5bface9747390ba201da1f176d99800d6a80ddb1
Invenblocker <barthipost@gmail.com>
2018-05-16 13:19:19 +0200

    Finished Appointmenet

--- SensumFileHandler/src/data/Appointment.java
+++ SensumFileHandler/src/data/Appointment.java
@@ -18,7 +18,7 @@ public class Appointment
     private Date date;
     private Patient patient;
     private ArrayList<StaffMember> staff;
-    private ArrayList<Associate> Associate;
+    private ArrayList<Associate> associates;
     private Institute institute;
     
     public Appointment(Date date, Patient patient, Collection<StaffMember> staff, Collection<Associate> associates, Institute institute)
@@ -26,8 +26,49 @@ public class Appointment
         this.date = date;
         this.patient = patient;
         this.staff = new ArrayList();
-        this.associate = new ArrayList();
+        this.associates = new ArrayList();
         
+        for(StaffMember staffMember : staff)
+        {
+            if(!this.staff.contains(staffMember)) this.staff.add(staffMember);
+        }
         
+        for(Associate associate : associates)
+        {
+            if(!this.associates.contains(associate)) this.associates.add(associate);
+        }
+        
+        this.institute = institute;
+    }
+    
+    public Date getDate()
+    {
+        return(date);
+    }
+    
+    public Patient getPatient()
+    {
+        return(patient);
+    }
+    
+    public StaffMember[] getStaff()
+    {
+        StaffMember[] clone = new StaffMember[staff.size()];
+        for(int i = 0; i < clone.length; i++) clone[i] = staff.get(i);
+        
+        return(clone);
+    }
+    
+    public Associate[] getAssociates()
+    {
+        Associate[] clone = new Associate[associates.size()];
+        for(int i = 0; i < clone.length; i++) clone[i] = associates.get(i);
+        
+        return(clone);
+    }
+    
+    public Institute getInstitute()
+    {
+        return(institute);
     }
 }
