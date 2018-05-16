12cc0af9761cb0f5635257d87de594d13be93ddd
Invenblocker <barthipost@gmail.com>
2018-05-16 12:33:50 +0200

    Added values to StaffMember

--- SensumFileHandler/src/data/StaffMember.java
+++ SensumFileHandler/src/data/StaffMember.java
@@ -5,10 +5,30 @@
  */
 package data;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 /**
  *
  * @author barth
  */
-public class StaffMember {
+public class StaffMember extends Person
+{
+    private StaffRole staffRole;
+    private HashSet<AccessClass> permissions, restrictions;
     
+    public StaffRole getStaffRole()
+    {
+        return(staffRole);
+    }
+    
+    public Collection<AccessClass> getPermissions()
+    {
+        return((HashSet)permissions.clone());
+    }
+    
+    public Collection<AccessClass> getRestrictions()
+    {
+        return((HashSet)restrictions.clone());
+    }
 }
