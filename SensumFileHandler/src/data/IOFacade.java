8755f1d46b163ff25e434d2b6de6ad152a77d11c
Invenblocker <barthipost@gmail.com>
2018-05-31 00:17:54 +0200

    Created enum for commands

--- SensumFileHandler/src/data/IOFacade.java
+++ SensumFileHandler/src/data/IOFacade.java
@@ -6,20 +6,37 @@
 package data;
 
 import communication.Input;
+import java.util.HashMap;
 
 /**
  *
  * @author barth
  */
-public class IOFacade
+public abstract class IOFacade
 {
+    private static HashMap<String, RecognizedCommands> commands;
+    
     public static String enterCommand(Input caller, String request)
     {
         return("Received command " + request + '.');
+      
     }
 
     public static Person attemptLogin(String mail, String password)
     {
         return(Person.login(mail, password));
+    }
+    
+    private static RecognizedCommands getCommand(String command)
+    {
+        if(commands == null)
+        {
+            commands = new HashMap();
+            for(RecognizedCommands currentCommand : RecognizedCommands.values())
+            {
+                commands.put(currentCommand.getName(), currentCommand);
+            }
+        }
+        return(commands.getOrDefault(command, null));
     }
 }
---
+++ SensumFileHandler/src/data/RecognizedCommands.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author barth
+ */
+public enum RecognizedCommands
+{
+    CREATE_ACCESSCLASS("createaccessclass");
+    
+    private String name;
+    
+    RecognizedCommands(String name)
+    {
+        this.name = name.toLowerCase();
+    }
+    
+    protected String getName()
+    {
+        return(name);
+    }
+}
